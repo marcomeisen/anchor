dae4f1f8aea2aa53106ced7db2d25531d89a9d15
marcomeisen <marcomeisen@gmail.com>
2026-07-31 23:10:41 +0200

    Initial Commit

---
+++ Anchor/AnchorApp.swift
@@ -0,0 +1,32 @@
+//
+//  AnchorApp.swift
+//  Anchor
+//
+//  Created by Marco Meisen on 31.07.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AnchorApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
